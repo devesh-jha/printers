dfef3af0b72896892f2e96f8d34e56775140d36d
devesh051 <iitiansmath@gmail.com>
2022-07-08 23:00:49 +0530

    rupee bug
    rupee bug

--- resources/views/backend/pages/sale/invoice.blade.php
+++ resources/views/backend/pages/sale/invoice.blade.php
@@ -347,19 +347,19 @@
             var tot2;
             var taxbytwo;
             tot1 = quantity * rate;
-
+                
             tot2 = tot1 * taxD;
-
+            
             result = tot1 + tot2;
-
+            result = (result.toFixed(2));
             taxbytwo = tax / 2;
             rateTax.innerHTML = taxbytwo + ' %';
 
             var br = document.createElement("br");
             rateTax.appendChild(br);
             rateTaxTwo.innerHTML = taxbytwo + ' %';
-            amtTax.innerHTML = tot2 / 2;
-            amtTax1.innerHTML = tot2 / 2;
+            amtTax.innerHTML = ((tot2 / 2).toFixed(2));
+            amtTax1.innerHTML = ((tot2 / 2).toFixed(2));
 
             var amountValue = document.getElementById("cal-amt-" + i);
             amountValue.innerHTML = result;
@@ -373,17 +373,19 @@
 
             totTax = totTax + tot2;
             var tottax = document.querySelector("#tottax")
-            tottax.innerHTML = totTax;
+            tottax.innerHTML = (totTax.toFixed(2));
 
-            totRes = totRes + result;
+            totRes = parseFloat(totRes) + parseFloat(result);
+            totRes=(totRes.toFixed(2));
+            
             var totres = document.querySelector("#totres")
             totres.innerHTML = totRes;
 
-            taxableVal.innerHTML = tot2;
+            taxableVal.innerHTML = (tot2.toFixed(2));
             var finaltaxTot =
                 finalAmt = totRes - advpay;
             var finalamt = document.querySelector("#finalamt");
-            finalamt.innerHTML = finalAmt;
+            finalamt.innerHTML = (finalAmt.toFixed(2));
             console.log(totRes);
             console.log(finalAmt);
             console.log(totTax);
@@ -411,30 +413,142 @@
 
 </script>
 <script>
-    var a = ['', 'One ', 'Two ', 'Three ', 'Four ', 'Five ', 'Six ', 'Seven ', 'Eight ', 'Nine ', 'Ten ', 'Eleven ',
-        'Twelve ', 'Thirteen ', 'Fourteen ', 'Fifteen ', 'Sixteen ', 'Seventeen ', 'Eighteen ', 'Nineteen '
-    ];
-    var b = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+    
 
-    function inWords(num) {
-        if ((num = num.toString()).length > 9) return 'overflow';
-        n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-        if (!n) return;
-        var str = '';
-        str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-        str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-        str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-        str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-        str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) +
-            'only ' : '';
-        return str;
+
+    function Rs(amount) {
+    var words = new Array();
+    words[0] = 'Zero';
+    words[1] = 'One';
+    words[2] = 'Two';
+    words[3] = 'Three';
+    words[4] = 'Four';
+    words[5] = 'Five';
+    words[6] = 'Six';
+    words[7] = 'Seven';
+    words[8] = 'Eight';
+    words[9] = 'Nine';
+    words[10] = 'Ten';
+    words[11] = 'Eleven';
+    words[12] = 'Twelve';
+    words[13] = 'Thirteen';
+    words[14] = 'Fourteen';
+    words[15] = 'Fifteen';
+    words[16] = 'Sixteen';
+    words[17] = 'Seventeen';
+    words[18] = 'Eighteen';
+    words[19] = 'Nineteen';
+    words[20] = 'Twenty';
+    words[30] = 'Thirty';
+    words[40] = 'Forty';
+    words[50] = 'Fifty';
+    words[60] = 'Sixty';
+    words[70] = 'Seventy';
+    words[80] = 'Eighty';
+    words[90] = 'Ninety';
+    var op;
+    amount = amount.toString();
+    var atemp = amount.split('.');
+    var number = atemp[0].split(',').join('');
+    var n_length = number.length;
+    var words_string = '';
+    if (n_length <= 9) {
+        var n_array = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
+        var received_n_array = new Array();
+        for (var i = 0; i < n_length; i++) {
+            received_n_array[i] = number.substr(i, 1);
+        }
+        for (var i = 9 - n_length, j = 0; i < 9; i++, j++) {
+            n_array[i] = received_n_array[j];
+        }
+        for (var i = 0, j = 1; i < 9; i++, j++) {
+            if (i == 0 || i == 2 || i == 4 || i == 7) {
+                if (n_array[i] == 1) {
+                    n_array[j] = 10 + parseInt(n_array[j]);
+                    n_array[i] = 0;
+                }
+            }
+        }
+        value = '';
+        for (var i = 0; i < 9; i++) {
+            if (i == 0 || i == 2 || i == 4 || i == 7) {
+                value = n_array[i] * 10;
+            } else {
+                value = n_array[i];
+            }
+            if (value != 0) {
+                words_string += words[value] + ' ';
+            }
+            if ((i == 1 && value != 0) || (i == 0 && value != 0 && n_array[i + 1] == 0)) {
+                words_string += 'Crores ';
+            }
+            if ((i == 3 && value != 0) || (i == 2 && value != 0 && n_array[i + 1] == 0)) {
+                words_string += 'Lakhs ';
+            }
+            if ((i == 5 && value != 0) || (i == 4 && value != 0 && n_array[i + 1] == 0)) {
+                words_string += 'Thousand ';
+            }
+            if (i == 6 && value != 0 && (n_array[i + 1] != 0 && n_array[i + 2] != 0)) {
+                words_string += 'Hundred and ';
+            } else if (i == 6 && value != 0) {
+                words_string += 'Hundred ';
+            }
+        }
+        words_string = words_string.split(' ').join(' ');
     }
+    return words_string;
+}
+
+function RsPaise(n) {
+    nums = n.toString().split('.')
+    var whole = Rs(nums[0])
+    if (nums[1] == null) nums[1] = 0;
+    if (nums[1].length == 1) nums[1] = nums[1] + '0';
+    if (nums[1].length > 2) {
+        nums[1] = nums[1].substring(2, length - 1)
+    }
+    if (nums.length == 2) {
+        if (nums[0] <= 9) {
+            nums[0] = nums[0] * 10
+        } else {
+            nums[0] = nums[0]
+        };
+        var fraction = Rs(nums[1])
+        if (whole == '' && fraction == '') {
+            op = 'Zero only';
+        }
+        if (whole == '' && fraction != '') {
+            op = 'paise ' + fraction + ' only';
+        }
+        if (whole != '' && fraction == '') {
+            op =   whole +' Rupees  only';
+        }
+        if (whole != '' && fraction != '') {
+            op =   whole +' Rupees and '+ fraction +'Paise Only';
+        }
+        console.log(op,"op");
+        // amt = document.getElementById('amt').value;
+        // if (amt > 999999999.99) {
+        //     op = 'Oops!!! The amount is too big to convert';
+        // }
+        // if (isNaN(amt) == true) {
+        //     op = 'Error : Amount in number appears to be incorrect. Please Check.';
+        // }
+        // console.log(op,"op");
+        return op;
+        
+    }
+}
+
+
+
+   
 
     NumToWord();
 
     function NumToWord() {
         var adpayval = document.querySelector('#totres');
-        document.querySelector("#words").innerText = inWords(adpayval.innerText);
+        document.querySelector("#words").innerText = RsPaise(adpayval.innerText);
     }
 
 </script>
